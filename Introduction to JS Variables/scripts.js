ab34194bc2d55372f8afbbe31ae106544407bd1b
niteshim0 <niteshim111@gmail.com>
2023-07-03 17:26:49 +0530

    JS Variables

---
+++ Introduction to JS Variables/scripts.js
@@ -0,0 +1,9 @@
+//Variables = they are the containers to store the value.
+var a  =  5;
+alert(a) ; //before ES6 var was used for declaring variables now this is of no more any use
+//let and const are now used for declaring variables
+let x = "Nitesh",y = 5;
+const t = 5;
+//t = 2;//TypeError const variables can't be reassigned
+//There is concept of hoisting as well we will see in future modules;
+//JS Statements : A instruction in program is known as statement.
