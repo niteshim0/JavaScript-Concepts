c69203be922162e20d9a1546e1085c16e3248723
niteshim0 <niteshim111@gmail.com>
2023-07-03 18:02:36 +0530

    variable notes added

--- Introduction to JS Variables/scripts.js
+++ Introduction to JS Variables/scripts.js
@@ -1,9 +1,11 @@
-//Variables = they are the containers to store the value.
+//Variables = A variable is a container for a value, like a number we might use in a sum, or a string that we might use as part of a sentence.
+/*One special thing about variables is that they can contain just about anything — not just strings and numbers. Variables can also contain complex data and even entire functions to do amazing things. You'll learn more about this as you go along.*/
 var a  =  5;
 alert(a) ; //before ES6 var was used for declaring variables now this is of no more any use
 //let and const are now used for declaring variables
 let x = "Nitesh",y = 5;
 const t = 5;
+//if variable is not intialized it contains the value undefined
 //t = 2;//TypeError const variables can't be reassigned
 //There is concept of hoisting as well we will see in future modules;
 //JS Statements : A instruction in program is known as statement.
